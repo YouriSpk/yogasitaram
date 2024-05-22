534c10b309ea51d33f00fd6fe2bd2ddb55f5c318
Youri Soposki <youri.soposki@gmail.com>
2024-05-22 11:07:35 +0200

    validation contact form

--- app/models/contact_form.rb
+++ app/models/contact_form.rb
@@ -1,2 +1,5 @@
 class ContactForm < ApplicationRecord
+  validates :name, presence: true
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :message, presence: true
 end
