d17706468451a6bde17e45f680c36eaaa0920609
Youri Soposki <youri.soposki@gmail.com>
2024-05-26 10:43:44 +0200

    progressing in controllers

--- app/controllers/pre_natals_controller.rb
+++ app/controllers/pre_natals_controller.rb
@@ -1,22 +1,71 @@
 class PreNatalsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
+  before_action :set_pre_natal, only: %i[ show edit update destroy ]
+
+  # GET /pre_natals or /pre_natals.json
   def index
+    @pre_natals = PreNatal.all
   end
 
+  # GET /pre_natals/1 or /pre_natals/1.json
   def show
   end
 
+  # GET /pre_natals/new
   def new
+    @pre_natal = PreNatal.new
   end
 
-  def create
-  end
-
+  # GET /pre_natals/1/edit
   def edit
   end
 
-  def update
+  # POST /pre_natals or /pre_natals.json
+  def create
+    @pre_natal = PreNatal.new(pre_natal_params)
+
+    respond_to do |format|
+      if @pre_natal.save
+        format.html { redirect_to pre_natals_path, notice: "Pre natal was successfully created." }
+        format.json { render :show, status: :created, location: @pre_natal }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @pre_natal.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
-  def destroy
+  # PATCH/PUT /pre_natals/1 or /pre_natals/1.json
+  def update
+    respond_to do |format|
+      if @pre_natal.update(pre_natal_params)
+        format.html { redirect_to pre_natals_path, notice: "Pre natal was successfully updated." }
+        format.json { render :show, status: :ok, location: @pre_natal }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @pre_natal.errors, status: :unprocessable_entity }
+      end
+    end
   end
+
+  # DELETE /pre_natals/1 or /pre_natals/1.json
+  def destroy
+    @pre_natal.destroy!
+
+    respond_to do |format|
+      format.html { redirect_to pre_natals_url, notice: "Pre natal was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_pre_natal
+      @pre_natal = PreNatal.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def pre_natal_params
+      params.require(:pre_natal).permit(:title, :description)
+    end
 end
