5a603f070739a72e41e17150e8f42bf59323305f
Youri Soposki <youri.soposki@gmail.com>
2024-05-26 10:39:48 +0200

    progressing in controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,54 +1,18 @@
 Rails.application.routes.draw do
-  get 'home_pages/index'
-  get 'home_pages/show'
-  get 'home_pages/new'
-  get 'home_pages/create'
-  get 'home_pages/edit'
-  get 'home_pages/update'
-  get 'home_pages/destroy'
-  get 'contacts/index'
-  get 'contacts/show'
-  get 'contacts/new'
-  get 'contacts/create'
-  get 'contacts/edit'
-  get 'contacts/update'
-  get 'contacts/destroy'
-  get 'schedules/index'
-  get 'schedules/show'
-  get 'schedules/new'
-  get 'schedules/create'
-  get 'schedules/edit'
-  get 'schedules/update'
-  get 'schedules/destroy'
-  get 'pre_natals/index'
-  get 'pre_natals/show'
-  get 'pre_natals/new'
-  get 'pre_natals/create'
-  get 'pre_natals/edit'
-  get 'pre_natals/update'
-  get 'pre_natals/destroy'
-  get 'yoga_styles/index'
-  get 'yoga_styles/show'
-  get 'yoga_styles/new'
-  get 'yoga_styles/create'
-  get 'yoga_styles/edit'
-  get 'yoga_styles/update'
-  get 'yoga_styles/destroy'
-  get 'teacher_cvs/index'
-  get 'teacher_cvs/show'
-  get 'teacher_cvs/new'
-  get 'teacher_cvs/create'
-  get 'teacher_cvs/edit'
-  get 'teacher_cvs/update'
-  get 'teacher_cvs/destroy'
+  # Utiliser resources pour générer les routes RESTful pour chaque modèle
+  resources :home_pages
+  resources :contacts, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+  resources :schedules
+  resources :pre_natals
+  resources :yoga_styles
+  resources :teacher_cvs
+
+  # Routes pour devise
   devise_for :users
+
+  # Route pour la page d'accueil
   root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  # Route pour le contrôle de santé de l'application
   get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_22_085312) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_24_074637) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,6 +30,13 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_22_085312) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_corporate_offers_on_user_id"
+  end
+
+  create_table "home_pages", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "offered_formats", force: :cascade do |t|
