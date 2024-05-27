0bfa5464e39d884f51ddadaa28f72837156a87ac
Youri Soposki <youri.soposki@gmail.com>
2024-05-27 20:39:45 +0200

    home page controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,15 @@
 class PagesController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:home]
+
   def home
+    @home_page = HomePage.last
+  end
+
+  def index
+    @home_pages = HomePage.all
+  end
+
+  def show
+    @home_page = HomePage.find(params[:id])
   end
 end
